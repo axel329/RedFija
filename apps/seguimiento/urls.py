dac5936a6e5ff677fb98aac9c57567b6c7da2a95
Alexander Torres <axel329@gmail.com>
2020-04-19 20:24:10 -0500

    Se modifica vista listarPQR dejando en la misma pagina el listado y creación de la pqr

--- apps/seguimiento/urls.py
+++ apps/seguimiento/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('editar_tipo/<int:pk>/', login_required(ActualizarTipo.as_view()), name='editar_tipo'),
     path('eliminar_tipo/<int:pk>/', login_required(EliminarTipo.as_view()), name='eliminar_tipo'),
     path('listar_pqr/', login_required(ListadoPqr.as_view()),name = 'listar_pqr'),
-    path('crear_pqr/', login_required(CrearPqr.as_view()),name = 'crear_pqr'),
+    #path('crear_pqr/', login_required(CrearPqr.as_view()),name = 'crear_pqr'),
     path('editar_pqr/<int:pk>/', login_required(ActualizarPqr.as_view()),name = 'editar_pqr'),
     path('eliminar_pqr/<int:pk>/', login_required(EliminarPqr.as_view()), name='eliminar_pqr'),
    
--- apps/seguimiento/views.py
+++ apps/seguimiento/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
-from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DeleteView
+from django.views.generic import View,  TemplateView, ListView, UpdateView, CreateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import TipoForm, PqrForm
 from .models import Tipo_seguimiento, Seguimiento
@@ -49,24 +49,42 @@ class EliminarTipo(DeleteView):
 
         return redirect('seguimiento:listar_tipo')
 
-class ListadoPqr(ListView):
-    model = Seguimiento
-    context_object_name = 'seguimientos'
-    template_name = 'seguimiento/proyectos/pqr/listar_pqr.html' 
-    queryset = Seguimiento.objects.filter(estado = True)
-
-class CrearPqr(CreateView):
+class ListadoPqr(View):
     model = Seguimiento
     form_class = PqrForm
-    template_name = 'seguimiento/proyectos/pqr/crear_pqr.html'
-    success_url = reverse_lazy('seguimiento:listar_pqr')
+    context_object_name = 'seguimientos'
+    template_name = 'seguimiento/proyectos/pqr/listar_pqr.html' 
+
+    def get_queryset(self):
+        return self.model.objects.filter(estado = True)
+
+    def get_context_data(self):
+        contexto = {}
+        contexto['seguimientos'] = self.get_queryset()
+        contexto['form'] = self.form_class
+        return contexto
+
+    def get(self, request, *args, **kwargs):
+       
+        return render(request, self.template_name, self.get_context_data())
+
+    def post(self,request,*args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('seguimiento:listar_pqr')
 
 
 class ActualizarPqr(UpdateView):
     model = Seguimiento
-    template_name = 'seguimiento/proyectos/pqr/crear_pqr.html'
+    template_name = 'seguimiento/proyectos/pqr/listar_pqr.html'
     form_class = PqrForm
     success_url = reverse_lazy('seguimiento:listar_pqr')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['seguimientos'] = Seguimiento.objects.filter(estado=True)
+        return context
 
 class EliminarPqr(DeleteView):
     model = Seguimiento
